c49e72091dfb4d1a1fed563616dfa465a623d3e4
ilmamfauzi <38044430+masilmam@users.noreply.github.com>
2022-03-15 10:51:07 +0700

    Solve logical test 4

---
+++ logical_test/logicalTest4.php
@@ -0,0 +1,18 @@
+<?php
+
+$i = 1;
+for($i; $i<=50; $i++) {
+  if($i % 3 == 0 && $i % 5 == 0) {
+    echo 'FooBar';
+  } else if($i % 3 == 0) {
+    echo 'Foo';
+  } else if($i % 5 == 0) {
+    echo 'Bar';
+  } else {
+    echo $i;
+  }
+  
+  echo ' ';
+}
+
+?>
